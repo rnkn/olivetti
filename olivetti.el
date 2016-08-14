9d318be86812055f6568f1ba4d899e245581eeba
Paul Rankin <hello@paulwrankin.com>
2016-08-14 20:34:01 +1000

    Added README to Commentary

--- olivetti.el
+++ olivetti.el
@@ -33,9 +33,9 @@
 ;; - Text body width can be the number of characters (an integer) or a
 ;;   fraction of the window width (a float between 0.0 and 1.0).
 ;; - Interactively change body width with:
-;;   `olivetti-shrink` C-c [
-;;   `olivetti-expand` C-c ]
-;;   and `olivetti-set-width`.
+;;   `olivetti-shrink` C-c [ [ [ ...
+;;   `olivetti-expand` C-c ] ] ] ...
+;;   `olivetti-set-width` C-c \
 ;; - If `olivetti-body-width` is an integer, the text body width will scale
 ;;   with use of `text-scale-mode`, whereas if a fraction (float) then the
 ;;   text body width will remain at that fraction.
@@ -66,7 +66,15 @@
 
 ;; - `linum-mode` currently has a bug that overwrites margin settings,
 ;;   making it incompatible with Olivetti. More information here:
-;;   <http://debbugs.gnu.org/cgi/bugreport.cgi?bug=20674>
+;;   <http://debbugs.gnu.org/20674>.
+;; - Emacs 25.1 introduced changes to `window-min-width` that return
+;;   erroneously large minimum window widths when using large window
+;;   margins, causing `split-window-right` to fail with a misleading
+;;   error message. This necessitates Olivetti patching `split-window-right`
+;;   to always split the window in half (in line with its documentation
+;;   string). This is designed as a temporary workaround until the Emacs
+;;   maintainers fix the problems with `window-min-width`.
+;;   See <http://debbugs.gnu.org/24193>.
 
 ;; Please report bugs on GitHub [Issues][] page.
 
