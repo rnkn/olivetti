46fdcfeeee026f23d4f835890454541d3335af8f
Paul W. Rankin <hello@paulwrankin.com>
2019-04-25 22:56:14 +1000

    Assign copyright to Free Software Foundation

--- olivetti.el
+++ olivetti.el
@@ -1,14 +1,13 @@
 ;;; olivetti.el --- Minor mode for a nice writing environment -*- lexical-binding: t; -*-
 
-;; Copyright (c) 2014-2017 Paul Rankin
+;; Copyright (c) 2014-2019  Free Software Foundation, Inc.
 
 ;; Author: Paul Rankin <hello@paulwrankin.com>
 ;; Keywords: wp, text
-;; Version: 1.6.1
+;; Version: 1.7.0
 ;; Package-Requires: ((emacs "24.5"))
-;; URL: https://github.com/rnkn/olivetti
 
-;; This file is not part of GNU Emacs.
+;; This file is part of GNU Emacs.
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -308,7 +307,7 @@ If prefixed with ARG, incrementally increase."
   "Mode map for `olivetti-mode'.")
 
 (define-obsolete-function-alias 'turn-on-olivetti-mode
-  'olivetti-mode "1.6.2")
+  'olivetti-mode "1.7.0")
 
 ;;;###autoload
 (define-minor-mode olivetti-mode
