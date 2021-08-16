9c5787321462e699162fcb807a1902d63cfee95c
Paul W. Rankin <pwr@bydasein.com>
2021-08-16 16:09:13 +1000

    Bump version

--- olivetti.el
+++ olivetti.el
@@ -4,7 +4,7 @@
 
 ;; Author: Paul W. Rankin <pwr@bydasein.com>
 ;; Keywords: wp, text
-;; Version: 2.0.0
+;; Version: 2.0.1
 ;; Package-Requires: ((emacs "24.4"))
 ;; URL: https://github.com/rnkn/olivetti
 
