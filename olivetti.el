0fd8bc929e50fce73a801cd59b0bc55a9f375fb5
Paul W. Rankin <rnkn@rnkn.xyz>
2021-10-30 18:38:10 +1000

    Bump version, update NEWS

--- olivetti.el
+++ olivetti.el
@@ -4,7 +4,7 @@
 
 ;; Author: Paul W. Rankin <pwr@bydasein.com>
 ;; Keywords: wp, text
-;; Version: 2.0.3
+;; Version: 2.0.4
 ;; Package-Requires: ((emacs "24.4"))
 ;; URL: https://github.com/rnkn/olivetti
 
